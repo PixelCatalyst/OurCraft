034405577c2b115f3a2360cc0597208f0a7b462c
Marcin Kienast <kienastmarcin@gmail.com>
2019-03-11 19:09:21 +0100

    Initial project version

---
+++ src/main/java/com/pixcat/core/Game.java
@@ -0,0 +1,67 @@
+package com.pixcat.core;
+
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+public class Game {
+    private long window;
+
+    private void run() {
+        System.out.println("Chyba działa XD");
+
+        init();
+        loop();
+        shutdown();
+    }
+
+    private void init() {
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        if (glfwInit() == false)
+            throw new IllegalStateException("Unable to initialize GLFW");
+
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+        window = glfwCreateWindow(300, 300, "OurCraft", NULL, NULL);
+        if (window == NULL)
+            throw new RuntimeException("Failed to create the GLFW window");
+
+        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+            if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+                glfwSetWindowShouldClose(window, true);
+        });
+
+        glfwMakeContextCurrent(window);
+        glfwSwapInterval(1);
+
+        glfwShowWindow(window);
+    }
+
+    private void loop() {
+        GL.createCapabilities();
+
+        while (!glfwWindowShouldClose(window)) {
+            glClearColor(((float) Math.sin(glfwGetTime()) + 1.0f) * 0.5f, 0.0f, 0.0f, 0.0f);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+    }
+
+    private void shutdown() {
+        glfwFreeCallbacks(window);
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+
+    public static void main(String[] args) {
+        new Game().run();
+    }
+}
