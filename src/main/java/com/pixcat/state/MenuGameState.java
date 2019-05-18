7cb2233592be6f30a44b2bcf26a48f0c387caa3b
Marcin Kienast <kienastmarcin@gmail.com>
2019-05-18 02:43:13 +0200

    Add initial menu drawing setup

--- src/main/java/com/pixcat/state/MenuGameState.java
+++ src/main/java/com/pixcat/state/MenuGameState.java
@@ -9,12 +9,18 @@ import static org.lwjgl.glfw.GLFW.GLFW_KEY_ESCAPE;
 public class MenuGameState implements GameState {
     private World world;
 
+    //TODO Background
+    //TODO Buttons
+    //TODO Button handling, state changes etc...
+
     public MenuGameState(World world) {
         this.world = world;
     }
 
     @Override
     public void draw(Renderer renderer) {
+        renderer.setBackgroundColor(0.31f, 0.55f, 0.82f);
+        renderer.setOrthographic();
         //TODO
     }
 
